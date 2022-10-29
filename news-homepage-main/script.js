799406c73dc0e389c68fbcc7c5d6220a01214668
mikhael <mikhaellion@gmail.com>
2022-10-29 15:25:15 +0700

    update

--- news-homepage-main/script.js
+++ news-homepage-main/script.js
@@ -1,10 +1,13 @@
 const sidebar = document.getElementById("sidebar");
 const btnMenu = document.getElementById("button-open");
 const btnClose = document.getElementById("button-close");
+const overlay = document.getElementById("overlay");
 
 btnMenu.addEventListener("click", () => {
   sidebar.classList.add("nav-show");
+  overlay.style.display = "block";
 });
 btnClose.addEventListener("click", () => {
   sidebar.classList.remove("nav-show");
+  overlay.style.display = "none";
 });
