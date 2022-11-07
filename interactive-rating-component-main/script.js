342080e09ebe9d12cea2bfba4dfb1a7fcba6fe7a
mikhael <mikhaellion@gmail.com>
2022-11-07 22:43:07 +0700

    update

---
+++ interactive-rating-component-main/script.js
@@ -0,0 +1,7 @@
+// const ratingList = document.getElementById("rating-list");
+// console.log("x", ratingList);
+// let x;
+// ratingList.button.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   console.log("e", e.target);
+// });
