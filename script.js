25b14b0b382a5360ae2f199c3d63264465649bd5
mikhael <mikhaellion@gmail.com>
2022-11-12 19:34:24 +0700

    update

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-// const ratingList = document.getElementById("rating-list");
-// console.log("x", ratingList);
-// let x;
-// ratingList.button.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   console.log("e", e.target);
-// });
+const ratingList = document.getElementById("rating-list");
+const ratingNum = document.querySelectorAll(".rating-rounded");
+const thankButton = document.getElementById("btn-submit");
+
+const thankContainer = document.getElementById("thank-you-card");
+const ratingContainer = document.getElementById("rating-card");
+
+// reference https://github.com/Alekususenpai/interactive-rating-component/blob/master/app.js
+
+for (let i = 0; i < ratingNum.length; i++) {
+  ratingNum[i].addEventListener("click", (item) => {
+    // 👇 if you want to get the value of the rating
+    // console.log("item", item.target.firstElementChild.value);
+    for (let i = 0; i < ratingNum.length; i++) {
+      ratingNum[i].classList.contains("isChecked");
+      ratingNum[i].classList.remove("isChecked");
+    }
+    ratingNum[i].classList.add("isChecked");
+  });
+}
+
+thankButton.addEventListener("click", () => {
+  thankContainer.classList.remove("hidden");
+  ratingContainer.classList.add("hidden");
+});
